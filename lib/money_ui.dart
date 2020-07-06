76da35dfd62a2da9b9c7cb5e2200264b9fa9b38a
David. J <codewithjalloh@gmail.com>
2020-07-06 06:33:46 +0100

    update code

---
+++ lib/money_ui.dart
@@ -0,0 +1,365 @@
+import 'package:flutter/material.dart';
+import 'detail_dashboard_ui.dart';
+import 'custom_shape_clipper.dart';
+import 'custom_upcoming_card.dart';
+
+class MoneyUI extends StatefulWidget {
+  @override
+  _MoneyUIState createState() => _MoneyUIState();
+}
+
+class _MoneyUIState extends State<MoneyUI> {
+  @override
+  Widget build(BuildContext context) {
+    Color primaryColor = Color.fromRGBO(255, 82, 48, 1);
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(244, 244, 244, 1),
+
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: primaryColor,
+                border: Border.all(
+                    color: primaryColor
+                ),
+              ),
+              child: Padding(
+                padding: EdgeInsets.only(top: 30.0, right: 15.0, left: 15.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.menu),
+                      color: Colors.white,
+                      iconSize: 30.0,
+                      onPressed: () {},
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.notifications_none),
+                      color: Colors.white,
+                      iconSize: 30.0,
+                      onPressed: () {},
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Stack(
+              children: [
+                ClipPath(
+                  clipper: CustomShapeClipper(),
+                  child: Container(
+                    height: 350,
+                    decoration: BoxDecoration(color: primaryColor),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                      horizontal: 25.0,
+                      vertical: 20.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "£32,000",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 30.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 15.0,),
+                          Text(
+                            "Jalloh: Current Balance",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 14.0
+                            ),
+                          ),
+                        ],
+                      ),
+                      Material(
+                        elevation: 1.0,
+                        borderRadius: BorderRadius.circular(100.0),
+                        color: Colors.blue[300],
+                        child: MaterialButton(
+                          onPressed: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (BuildContext context) => DetailDashboardPage()));
+                          },
+                          padding: EdgeInsets.symmetric(
+                              vertical: 10.0,
+                              horizontal: 30.0),
+                          child: Text(
+                            "TOP UP",
+                            style: TextStyle(
+                                fontSize: 16.0,
+                                color: Colors.white
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: 120.0,
+                    right: 25.0,
+                    left: 25.0,
+                  ),
+                  child: Container(
+                    width: double.infinity,
+                    height: 370,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.black.withOpacity(0.0),
+                              offset: Offset(0.0, 3.0),
+                              blurRadius: 15.0)
+                        ]),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 40.0,
+                              vertical: 40.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                children: [
+                                  Material(
+                                    borderRadius: BorderRadius.circular(100.0),
+                                    color: Colors.purple.withOpacity(0.1),
+                                    child: IconButton(
+                                      padding: EdgeInsets.all(15.0),
+                                      icon: Icon(Icons.send),
+                                      iconSize: 30.0,
+                                      onPressed: () {},
+                                    ),
+                                  ),
+                                  SizedBox(height: 8.0),
+                                  Text("Send",
+                                      style: TextStyle(
+                                          color: Colors.black54,
+                                          fontWeight: FontWeight.bold))
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  Material(
+                                    borderRadius: BorderRadius.circular(100.0),
+                                    color: Colors.blue.withOpacity(0.1),
+                                    child: IconButton(
+                                      padding: EdgeInsets.all(15.0),
+                                      icon: Icon(Icons.credit_card),
+                                      color: Colors.blue,
+                                      iconSize: 30.0,
+                                      onPressed: () {},
+                                    ),
+                                  ),
+                                  SizedBox(height: 8.0,),
+                                  Text("Pay",
+                                    style: TextStyle(
+                                        color: Colors.black54,
+                                        fontWeight: FontWeight.bold
+                                    ),)
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  Material(
+                                    borderRadius: BorderRadius.circular(100.0),
+                                    color: Colors.orange.withOpacity(0.1),
+                                    child: IconButton(
+                                      padding: EdgeInsets.all(15.0),
+                                      icon: Icon(Icons.receipt),
+                                      iconSize: 30.0,
+                                      onPressed: () {},
+                                    ),
+                                  ),
+                                  SizedBox(height: 8.0,),
+                                  Text("Request",
+                                    style: TextStyle(
+                                        color: Colors.black54,
+                                        fontWeight: FontWeight.bold
+                                    ),),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 40.0
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                children: [
+                                  Material(
+                                    borderRadius: BorderRadius.circular(100.0),
+                                    color: Colors.pink.withOpacity(0.1),
+                                    child: IconButton(
+                                      padding: EdgeInsets.all(15.0),
+                                      icon: Icon(Icons.monetization_on),
+                                      color: Colors.pink,
+                                      iconSize: 30.0,
+                                      onPressed: () {},
+                                    ),
+                                  ),
+                                  SizedBox(height: 8.0,),
+                                  Text("Invoice",
+                                    style: TextStyle(
+                                        color: Colors.black54,
+                                        fontWeight: FontWeight.bold
+                                    ),),
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  Material(
+                                    borderRadius: BorderRadius.circular(100.0),
+                                    color: Colors.purpleAccent.withOpacity(0.1),
+                                    child: IconButton(
+                                      padding: EdgeInsets.all(15.0),
+                                      icon: Icon(Icons.favorite),
+                                      color: Colors.purpleAccent,
+                                      iconSize: 30.0,
+                                      onPressed: () {},
+                                    ),
+                                  ),
+                                  SizedBox(height: 8.0,),
+                                  Text("Charity",
+                                    style:TextStyle(
+                                        color: Colors.black54,
+                                        fontWeight: FontWeight.bold
+                                    ) ,)
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  Material(
+                                    borderRadius: BorderRadius.circular(100.0),
+                                    color: Colors.deepPurple.withOpacity(0.1),
+                                    child: IconButton(
+                                      padding: EdgeInsets.all(15.0),
+                                      icon: Icon(Icons.attach_money),
+                                      color: Colors.deepPurple,
+                                      iconSize: 30.0,
+                                      onPressed: () {},
+                                    ),
+                                  ),
+                                  SizedBox(height: 8.0,),
+                                  Text("Loan",
+                                    style: TextStyle(
+                                        color: Colors.black54,
+                                        fontWeight: FontWeight.bold
+                                    ) ,
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(height: 15.0),
+                        Divider(),
+                        SizedBox(height: 15.0),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 25.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Expanded(
+                                child: Text(
+                                  "Lorem ipsum dolor sit amet dan aku tan moyan",
+                                  textAlign: TextAlign.left,
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(width: 40.0,),
+                              Material(
+                                borderRadius: BorderRadius.circular(100.0),
+                                color: Colors.blueAccent.withOpacity(0.1),
+                                child: IconButton(
+                                  icon: Icon(Icons.arrow_back_ios),
+                                  color: Colors.blueAccent,
+                                  onPressed: () {},
+
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 25.0,
+                vertical: 30.0,
+              ),
+              child: Text("Upcoming",
+                style: TextStyle(
+                  color: Colors.black.withOpacity(0.7),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0,
+                ),),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                  left: 35.0, bottom: 25.0
+              ),
+              child: Container(
+                height: 150.0,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    UpComingCard(
+                      title: "Credit Card One",
+                      value: 280.0,
+                      color: Colors.purple,
+                    ),
+                    UpComingCard(
+                      title: "Credit Card Two",
+                      value: 260.0,
+                      color: Colors.blue,
+                    ),
+                    UpComingCard(
+                      title: "Credit Card Two",
+                      value: 260.0,
+                      color: Colors.orange,
+                    ),
+                    UpComingCard(
+                      title: "Credit Card Two",
+                      value: 260.0,
+                      color: Colors.pink,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
